f89b4db37961d4554e7c2498d89d5eb2cb739267
Anna <fugaon@mail.ru>
2016-02-01 11:56:59 +0300

    oliwcss 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp 			= require('gulp'),
 
 	livereload 		= require('gulp-livereload'),
-	connect 		= require('gulp-connect'),
 	sourcemaps 		= require('gulp-sourcemaps'),
 	rename 			= require("gulp-rename"),
 
 	less 			= require('gulp-less'),
 
 	autoprefixer 	= require('gulp-autoprefixer'),
-	minifyCSS		= require('gulp-minify-css'),
+	cssnano			= require('gulp-cssnano'),
 	uncss 			= require('gulp-uncss'),
 
 	jade 			= require('gulp-jade'),
@@ -17,299 +16,149 @@ var gulp 			= require('gulp'),
 
 
 
-gulp.task('connect', function() {
-
-	connect.server ({
-		root: 'desktop/code/dist/',
-		livereload: true
-	});
-});
-
-
-
 gulp.task('css', function() {
-	
-	gulp.src('desktop/code/app/less/**/all.less')
 
-		.pipe (
-			sourcemaps.init()
-		)
-
-		.pipe (
-			less ()
-		)
-
-		.pipe (
-			autoprefixer ({
-				browsers: ['> 1%', 'IE 9'],
-				cascade: false
-			})
-		)
-
-		.pipe (
-			minifyCSS({
-				compatibility: 'ie8'
-			})
-		)
-
-		.pipe (
-			rename({
-				suffix: '.min'
-			})
-		)
-
-		.pipe (
-			sourcemaps.write('./')
-		)
-
-		.pipe(
-			gulp.dest('desktop/code/dist/css/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+	css('desktop/code/app/less/**/all.less', 'desktop/code/dist/css/');
 });
-
-
 
 gulp.task('css_tb', function() {
 	
-	gulp.src('tablet/code/app/less/**/all.less')
-
-		.pipe (
-			sourcemaps.init()
-		)
-
-		.pipe (
-			less ()
-		)
-
-		.pipe (
-			autoprefixer ({
-				browsers: ['> 1%', 'IE 9'],
-				cascade: false
-			})
-		)
-
-		.pipe (
-			minifyCSS({
-				compatibility: 'ie8'
-			})
-		)
-
-		.pipe (
-			rename({
-				suffix: '.min'
-			})
-		)
-
-		.pipe (
-			sourcemaps.write('./')
-		)
-
-		.pipe(
-			gulp.dest('tablet/code/dist/css/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+	css('tablet/code/app/less/**/all.less', 'tablet/code/dist/css/');
 });
-
-
 
 gulp.task('css_mb', function() {
 	
-	gulp.src('mobile/code/app/less/**/all.less')
-
-		.pipe (
-			sourcemaps.init()
-		)
-
-		.pipe (
-			less ()
-		)
-
-		.pipe (
-			autoprefixer ({
-				browsers: ['> 1%', 'IE 9'],
-				cascade: false
-			})
-		)
-
-		.pipe (
-			minifyCSS({
-				compatibility: 'ie8'
-			})
-		)
-
-		.pipe (
-			rename({
-				suffix: '.min'
-			})
-		)
-
-		.pipe (
-			sourcemaps.write('./')
-		)
-
-		.pipe(
-			gulp.dest('mobile/code/dist/css/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+	css('mobile/code/app/less/**/all.less', 'mobile/code/dist/css/');
 });
+
 
 
 
 gulp.task('html', function() {
 
-	gulp.src([
+	html([
 		'desktop/code/app/jade/**/*.jade',
-		'!desktop/code/app/jade/includes/**/*.jade' ])
-
-		.pipe (
-			jade({
-				pretty: true
-			})
-		)
-
-		.pipe(
-			gulp.dest('desktop/code/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+		'!desktop/code/app/jade/includes/**/*.jade' ], 'desktop/code/');
 });
 
 gulp.task('html_tb', function() {
 
-	gulp.src([
+	html([
 		'tablet/code/app/jade/**/*.jade',
-		'!tablet/code/app/jade/includes/**/*.jade' ])
-
-		.pipe (
-			jade({
-				pretty: true
-			})
-		)
-
-		.pipe(
-			gulp.dest('tablet/code/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+		'!tablet/code/app/jade/includes/**/*.jade' ], 'tablet/code/');
 });
 
 gulp.task('html_mb', function() {
 
-	gulp.src([
+	html([
 		'mobile/code/app/jade/**/*.jade',
-		'!mobile/code/app/jade/includes/**/*.jade' ])
-
-		.pipe (
-			jade({
-				pretty: true
-			})
-		)
-
-		.pipe(
-			gulp.dest('mobile/code/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+		'!mobile/code/app/jade/includes/**/*.jade' ], 'mobile/code/');
 });
+
 
 
 
 gulp.task('js', function() {
 
-	gulp.src('desktop/code/app/js/specific/**/*.jade')
-
-		.pipe (
-			uglify()
-		)
-
-		.pipe(
-			gulp.dest('desktop/code/dist/js/specific/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+	js('desktop/code/app/js/**/*.js', 'desktop/code/dist/js/');
 });
 
 gulp.task('js_tb', function() {
 
-	gulp.src('tablet/code/app/js/specific/**/*.jade')
-
-		.pipe (
-			uglify()
-		)
-
-		.pipe(
-			gulp.dest('tablet/code/dist/js/specific/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
+	js('tablet/code/app/js/**/*.js', 'tablet/code/dist/js/');
 });
 
 gulp.task('js_mb', function() {
 
-	gulp.src('mobile/code/app/js/specific/**/*.jade')
-
-		.pipe (
-			uglify()
-		)
-
-		.pipe(
-			gulp.dest('mobile/code/dist/js/specific/')
-		)
-
-		.pipe(
-			connect.reload()
-		);
-});
-
-
-
-gulp.task('default', function () {
-	return gulp.src('site.css')
-		.pipe(
-			uncss({
-				html: ['*.html']
-			})
-		)
-		.pipe(
-			gulp.dest('./out')
-		);
+	js('mobile/code/app/js/**/*.js', 'mobile/code/dist/js/');
 });
 
 
 
 gulp.task('watch', function() {
 
-	gulp.watch('desktop/code/app/less/**/*.less', ['css']);
-	gulp.watch('tablet/code/app/less/**/*.less', ['css_tb']);
-	gulp.watch('mobile/code/app/less/**/*.less', ['css_mb']);
+	gulp.watch('desktop/code/app/less/**/*', ['css']);
+	gulp.watch('tablet/code/app/less/**/*', ['css_tb']);
+	gulp.watch('mobile/code/app/less/**/*', ['css_mb']);
 
-	gulp.watch('desktop/code/app/**/*.jade', ['html']);
-	gulp.watch('tablet/code/app/**/*.jade', ['html_tb']);
-	gulp.watch('mobile/code/app/**/*.jade', ['html_mb']);
+	gulp.watch('desktop/code/app/jade/**/*', ['html']);
+	gulp.watch('tablet/code/app/jade/**/*', ['html_tb']);
+	gulp.watch('mobile/code/app/jade/**/*', ['html_mb']);
 
-	gulp.watch('desktop/code/app/js/specific/**/*.js', ['js']);
-	gulp.watch('tablet/code/app/js/specific/**/*.js', ['js_tb']);
-	gulp.watch('mobile/code/app/js/specific/**/*.js', ['js_mb']);
+	gulp.watch('desktop/code/app/js/**/*.js', ['js']);
+	gulp.watch('tablet/code/app/js/**/*.js', ['js_tb']);
+	gulp.watch('mobile/code/app/js/**/*.js', ['js_mb']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['watch']);
+
+
+
+
+
+function css(from, to) {
+	gulp.src(from)
+
+		.pipe (
+			sourcemaps.init()
+		)
+
+		.pipe (
+			less()
+		)
+
+		.pipe (
+			autoprefixer ({
+				browsers: ['> 1%', 'IE 9'],
+				cascade: false
+			})
+		)
+
+		.pipe (
+			cssnano({
+				zindex: false
+			})
+		)
+
+		.pipe (
+			rename({
+				suffix: '.min'
+			})
+		)
+
+		.pipe (
+			sourcemaps.write('./')
+		)
+
+		.pipe(
+			gulp.dest(to)
+		);
+}
+
+function html(from, to) {
+
+	gulp.src(from)
+
+		.pipe (
+			jade({
+				pretty: true
+			})
+		)
+
+		.pipe(
+			gulp.dest(to)
+		);
+}
+
+function js(from, to) {
+
+	gulp.src(from)
+
+		.pipe (
+			uglify()
+		)
+
+		.pipe(
+			gulp.dest(to)
+		);
+}
